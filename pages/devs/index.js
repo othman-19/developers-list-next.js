5999ac0247e8e5cd1101aad9869cd1b58c42ad8a
othmane <othman-19@hotmail.fr>
2021-05-09 00:04:27 +0100

    fetching and styling the developers list

--- pages/devs/index.js
+++ pages/devs/index.js
@@ -1,17 +1,31 @@
-import styles from '../../styles/Home.module.css'
+import styles from '../../styles/Devs.module.css'
 
-const Devs = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  return {
+    props: {
+      developers: data
+    }
+  }
+};
+
+const Devs = ({ developers }) => {
   return (
-    <div className={styles.container}>
+    <div>
       <h1>
         Developers List
       </h1>
-      <p>
-        Lorem ipsum paragraphe 1
-      </p>
-      <p>
-        Lorem ipsum paragraphe 2
-      </p>
+      {developers.map(developer => (
+        <div key={developer.id}>
+          <a className={styles.single}>
+            <h3>
+              {developer.name}
+            </h3>
+          </a>
+
+        </div>
+      ))}
     </div>
   );
 }
